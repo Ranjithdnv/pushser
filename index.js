13946bc89d5d0864f71367e0ae969f000fd1d4ba
Ranjithdnv <rith8596@gmail.com>
2023-09-28 02:26:17 +0530

    h

--- index.js
+++ index.js
@@ -23,29 +23,8 @@ webpush.setVapidDetails(
   apiKeys.privateKey
 );
 console.log(0);
-const subDatabse = [
-  // {
-  //   endpoint:
-  //     "https://fcm.googleapis.com/fcm/send/en_Em5-jHJw:APA91bGhtavH2UoFp1YUgQX6Q99MHT7f491jonjKlgcyeWLdqZnGQCQA35rNJZVOCxgAKX8gMH-96mv1wpi21vYf0VJcHNmrizsUcWoszL7mU8RgXjIVRoDJ2dXE92x_GgZ0QiCd2dJ8",
-  //   expirationTime: null,
-  //   keys: {
-  //     p256dh:
-  //       "BB0IrPkMRgdZYW0Y120IhjA21jYbSTIybVO8xp0dxdCS-Qgc34dGP9571wwI4wyK7UkRMj3TSjEt2H1NjCN0x7E",
-  //     auth: "ugMp2KfAs_LOy-fH70bz3rHkLbLSZEu2OaaUOf_My7s",
-  //   },
-  // },
-];
+const subDatabse = [];
 let message = "";
-//-----------------------------------------------
-// mongoose
-//   .connect(
-//     "mongodb+srv://pavankumarmoka:3ccG3rpxQoWOGEJl@expresscluster.gfleory.mongodb.net/Bakery?retryWrites=true&w=majority",
-//     {
-//       useNewUrlParser: true,
-//       useUnifiedTopology: true,
-//     }
-//   )
-//   .then(() => console.log("success"));
 
 //------------------
 
@@ -78,24 +57,12 @@ app.post("/subscribe", async (req, res) => {
     };
   }
 });
-// .catch((err) => {
-//   console.log(err.message);
-// });
 
 app.get("/send-notification", (req, res) => {
   webpush.sendNotification(subDatabse[0], "Hello world");
   res.json({ statue: "Success", message: "Message sent to push service" });
 });
 //
-app.delete("/deleteitems", async (req, res) => {
-  try {
-    // console.log(req)
-    await Items.deleteMany();
-    res.status(200).json({ delete: "done" });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
 
 //
 app.listen(3003, () => {
